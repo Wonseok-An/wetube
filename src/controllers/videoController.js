08be9a78dd19223e40033e7c0224b4ed1dc5e399
Wonseok An <wonseok.an95@gmail.com>
2022-05-12 02:17:45 +0900

    20220511

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,47 +1,62 @@
-const videos = [
-    {
-        title: "First Video",
-        rating: 5,
-        comments: 2,
-        createdAt: "2 minutes ago",
-        views: 59,
-        id: 1
-    },
-    {
-        title: "Second Video",
-        rating: 5,
-        comments: 2,
-        createdAt: "2 minutes ago",
-        views: 59,
-        id: 2
-    },
-    {
-        title: "Third Video",
-        rating: 5,
-        comments: 2,
-        createdAt: "2 minutes ago",
-        views: 1,
-        id: 3
-    },
-]
+import Video from "../models/Video";
 
-export const trending = (req, res) => {
-    return res.render("home", {pageTitle: "Home", videos});
+export const home = async (req, res) => {
+    try {
+        const videos = await Video.find({});
+        return res.render("home", {pageTitle: "Home", videos});
+    } catch (error) {
+        console.log(error);
+        return res.send("server-error");
+    }
 }
-export const watch = (req, res) => {
-    const { id } = req.params;
-    const video = videos.filter((obj) => String(obj.id) === id)[0];
-    return res.render("watch", {pageTitle: `Watching ${video.title}`, video});
+export const watch = async (req, res) => {
+    const {id} = req.params;
+    const video = await Video.findById(id);
+    if (!video) {
+        return res.render("404", {pageTitle: "Video not found."});
+    }
+    return res.render("watch", {pageTitle: video.title, video});
 }
-export const getEdit = (req, res) => {
-    const { id } = req.params;
-    const video = videos.filter((obj) => String(obj.id) === id)[0];
-    return res.render("edit", {pageTitle: `Editing ${video.title}`, video});
+export const getEdit = async (req, res) => {
+    const {id} = req.params;
+    const video = await Video.findById(id);
+    if (!video) {
+        return res.render("404", {pageTitle: "Video not found."});
+    }
+    return res.render("edit", {pageTitle: `Edit ${video.title}`, video});
 }
-export const postEdit = (req, res) => {
-    const { id } = req.params;
-    const video = videos.filter((obj) => String(obj.id) === id)[0];
-    video.title = req.body.title;
-
+export const postEdit = async (req, res) => {
+    const {id} = req.params;
+    const {title, description, hashtags} = req.body;
+    const video = await Video.exists({_id: id});
+    if (!video) {
+        return res.render("404", {pageTitle: "Video not found."});
+    }
+    await Video.findByIdAndUpdate(id, {
+        title,
+        description,
+        hashtags: Video.formatHashtags(hashtags)
+    })
     return res.redirect(`/videos/${id}`)
+}
+export const getUpload = (req, res) => {
+    return res.render("upload", {pageTitle: "Upload Video"});
+}
+export const postUpload = async (req, res) => {
+    const {title, description, hashtags} = req.body;
+    try {
+        await Video.create({
+            title,
+            description,
+            hashtags: Video.formatHashtags(hashtags)
+        });
+        return res.redirect("/");
+    } catch (error) {
+        return res.render("upload", {pageTitle: "Upload Video", errorMessage: error._message});
+    }
+}
+export const deleteVideo = async (req, res) => {
+    const {id} = req.params;
+    await Video.findByIdAndDelete(id);
+    return res.redirect("/")
 }
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,9 +1,10 @@
 import express from "express";
-import {watch, getEdit, postEdit} from "../controllers/videoController";
+import {watch, getEdit, postEdit, getUpload, postUpload, deleteVideo} from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
-videoRouter.get("/:id(\\d+)", watch);
-videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
-
+videoRouter.get("/:id([0-9a-f]{24})", watch);
+videoRouter.route("/:id([0-9a-f]{24})/edit").get(getEdit).post(postEdit);
+videoRouter.get("/:id([0-9a-f]{24})/delete", deleteVideo);
+videoRouter.route("/upload").get(getUpload).post(postUpload);
 export default videoRouter;
