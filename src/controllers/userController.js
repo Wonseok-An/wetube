10f4ac19d1e6be56b495e6a9944ef75ab0187686
Wonseok An <wonseok.an95@gmail.com>
2022-06-01 14:13:30 +0900

    20220601-Finish Section15

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -109,6 +109,7 @@ export const finishGithubLogin = async (req, res) => {
 
 export const logout = (req, res) => {
     req.session.destroy();
+    req.flash("info", "Bye Bye");
     return res.redirect("/")
 };
 
@@ -147,6 +148,7 @@ export const postEdit = async (req, res) => {
 
 export const getChangePassword = (req, res) => {
     if (req.session.user.socialOnly === true) {
+        req.flash("error", "Can't change password.");
         return res.redirect("/")
     }
     return res.render("users/change-password", {pageTitle: "Change Password"})
@@ -174,6 +176,7 @@ export const postChangePassword = async (req, res) => {
     user.password = newPassword;
     await user.save();
     req.session.destroy();
+    req.flash("info", "Password Updated");
     return res.redirect("/login");
 }
 
--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -26,6 +26,7 @@ export const getEdit = async (req, res) => {
         return res.status(404).render("404", {pageTitle: "Video not found."});
     }
     if (String(video.owner) !== _id) {
+        req.flash("error", "Not authorized");
         return res.status(403).redirect("/");
     }
     return res.render("edit", {pageTitle: `Edit ${video.title}`, video});
@@ -39,6 +40,7 @@ export const postEdit = async (req, res) => {
         return res.status(404).render("404", {pageTitle: "Video not found."});
     }
     if (String(video.owner) !== String(_id)) {
+        req.flash("error", "You are not the owner of the video.");
         return res.status(403).redirect("/");
     }
     await Video.findByIdAndUpdate(id, {
@@ -46,6 +48,7 @@ export const postEdit = async (req, res) => {
         description,
         hashtags: Video.formatHashtags(hashtags)
     })
+    req.flash("success", "Changes saved.")
     return res.redirect(`/videos/${id}`)
 }
 export const getUpload = (req, res) => {
@@ -54,7 +57,7 @@ export const getUpload = (req, res) => {
 export const postUpload = async (req, res) => {
     const {
         body: {title, description, hashtags},
-        file: { path },
+        files: { video, thumb },
         session: {
             user: { _id:owner }
         }
@@ -65,7 +68,8 @@ export const postUpload = async (req, res) => {
             title,
             description,
             hashtags: Video.formatHashtags(hashtags),
-            fileUrl: path,
+            fileUrl: video[0].path,
+            thumbUrl: thumb[0].path,
             owner
         });
         const user = await User.findById(owner);
